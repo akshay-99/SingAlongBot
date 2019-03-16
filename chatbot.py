0bbf8325858aabeeb1b6dcc42700c8ec5fe7599d
Akshay Padte <akshaypadte99@gmail.com>
2019-03-17 01:59:28 +0530

    Remove sockets :/

--- chatbot.py
+++ chatbot.py
@@ -1,14 +1,13 @@
 from flask import Flask, request, jsonify, render_template
-from flask_socketio import SocketIO
 import json
 import requests
-import time
+
 import random
 import string
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ghrg4854yn754ct5cvrtgtAS'
-socketio = SocketIO(app)
+
 PAT = 'EAAElanWHWs0BAGrlESwCtGH6C4ZAtfGtINDVPgcWiuUQo3ZAeY0O7H4ic1b1lPmVgcmqpjYPxom0V80cZBD4K5tvdaiwQB3dnFVUvZAgJQlSo1rLEmxHH8Ojuq1LAkoyM300upWo9HZAlRNO91LZBURLJsRDOQH3pnBNjZBOYQKK1cQ95ZA9TNyg'
 print('rerun')
 
@@ -17,55 +16,28 @@ project_id = 'test-chatbot-53656'
 session_id = '83ebfe95-d844-1525-de20-9d15059ec6a8'
 
 
-@socketio.on('user sent')
-def receive_message(json, methods=['GET', 'POST']):
-    print('received my event: ' + str(json))
-    print(request.namespace)
-    contents = {'sender':request.sid, 'body':json['msg']}
-    print(contents)
-    reply(contents)
+@app.route('/send',methods=['POST'])
+def receive_message():
+    data = request.json
+    replies = generate_reply(data['msg'])
+    return jsonify({'count': len(replies), 'msgs': replies })
 
-def reply(msg, facebook=False):
+def generate_reply(msg):
     '''
     Process and reply
     '''
-    msg_body = msg['body']
-    my_reply = detect_intent_texts(project_id, session_id, [msg_body], 'en')
-    sender = msg['sender']
-    send_message(sender, my_reply, facebook=facebook, token=PAT)
-
-def sent_success():
-    print('client received the response')
-def send_message(recipient, text, facebook=False, token=None):
-    """Send the message text to recipient with id recipient.
-    """
-    if not facebook:
-        print('\n\nresponding...\n')
-        socketio.emit('reply', {'msg':text},room=recipient,namesplace='/', callback=sent_success)
-    else:
-        sendfacebookmessage(recipient, text, token)
-
+    replies = detect_intent_texts(project_id, session_id, [msg], 'en')
+    return replies
 
 @app.route('/chatbot')
 def chatbot():
     return render_template('chatbot.html')
 
-
-
-# ------------------------- calls to Dialog flow ------------------------------
+# ------------------------- calls to and from Dialog flow ------------------------------
 def detect_intent_texts(project_id, session_id, texts, language_code):
     import dialogflow_v2 as dialogflow
-    """Returns the result of detect intent with texts as inputs.
-
-    Using the same `session_id` between requests allows continuation
-    of the conversation."""
-
-    
     session_client = dialogflow.SessionsClient()
-
     session = session_client.session_path(project_id, session_id)
-    print('Session path: {}\n'.format(session))
-
     for text in texts:
         text_input = dialogflow.types.TextInput(
             text=text, language_code=language_code)
@@ -74,15 +46,8 @@ def detect_intent_texts(project_id, session_id, texts, language_code):
 
         response = session_client.detect_intent(
             session=session, query_input=query_input)
-
-        print('=' * 20)
-        print('Query text: {}'.format(response.query_result.query_text))
-        print('Detected intent: {} (confidence: {})\n'.format(
-            response.query_result.intent.display_name,
-            response.query_result.intent_detection_confidence))
-        print('Fulfillment text: {}\n'.format(
-            response.query_result.fulfillment_text)) 
-    return response.query_result.fulfillment_text
+         
+    return [response.query_result.fulfillment_text]
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
@@ -136,14 +101,10 @@ def handle_messages_fb():
     for sender, message in messaging_events(payload):
         print ("Incoming from %s: %s" % (sender, message))
         # send_message(PAT, sender, message)
-        msg = {'sender':sender, 'body':message}
-        reply(msg, facebook=True)
+        
+        sendfacebookmessage(sender, message, PAT)
     return "ok"
 
-@app.route('/sleep')
-def long():
-    time.sleep(10)
-    return 'done'
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    app.run(debug=True, threaded=True)
--- static/js/app.js
+++ static/js/app.js
@@ -2,25 +2,7 @@
 var sender_id=null
 var socket=null;
 $('body').ready(function() {
-    $('#ta-msg')[0].disabled=true;
-    $('#ta-msg')[0].value="Connecting...";
-    var prefix = 'https://';
-    if (location.protocol != 'https:')
-    {
-        prefix = 'http://'
-    }
     
-    socket = io.connect(prefix + document.domain + ':' + location.port);
-    
-    socket.on('connect', ()=>{
-        $('#ta-msg')[0].disabled=false;
-        $('#ta-msg')[0].value="";
-        console.log('connected');
-    });
-    socket.on( 'reply', function(msg)
-    {
-        res(msg.msg);
-    });
 });
 
 function sender() {
@@ -28,10 +10,20 @@ function sender() {
     $('.main').append('<div class="txt-div" ><div class="txt msg">'+msg+'</div><div class="time">'+new Date().toLocaleTimeString().slice(0, -3)+'</div></div>');
     $('.txt1').val('');
     $('.chat').scrollTop = $('.chat').scrollHeight;
-    
-    socket.emit( 'user sent', {
-        
-        msg : msg
+    url = '/send';
+    tosend = {msg: msg};
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(tosend)
+    }).then(response=>response.json())
+    .then(json => {
+        msgs = json.msgs;
+        console.log(msgs);
+        msgs.forEach(res);
     });
 }
   
